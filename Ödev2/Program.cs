9f0c6588e3c578035e9ab6216c1beccfc3c52c81
doguk <doguk@DOUKAN>
2024-02-11 20:33:39 +0300

    Proje dosyası ekle.

---
+++ Ödev2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person person1 = new Person();
+person1.FirstName = "Doğukan";
+person1.LastName = "Atar";
+person1.DateOfBirthYear = 2002;
+person1.tcNo = 12345678910;
+
+PttManager pttmanager = new PttManager(new PersonManager());
+pttmanager.GiveMask(person1);
